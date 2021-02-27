f5cda469dd99e17f26f661add77ef0c378940e4d
TM0D <37343661+TM0D@users.noreply.github.com>
2021-02-26 18:34:57 -0600

    initial commit

---
+++ openText.js
@@ -0,0 +1,31 @@
+
+
+
+
+/*
+var randomText = "ABCDEFGHJKLMNOPQRSTUVWXYZ23456789"
+var time = 0
+
+
+for (let i = 0; i < 1000; i++) {
+    let returnText = "";
+
+    if (i > 999) {
+        returnText = "TM0D";
+    } else if (i > 749) {
+        returnText = "TM0"
+    } else if (i > 499) {
+        returnText = "TM"
+    } else if (i > 249) {
+        returnText = "T"
+    }
+    
+    for (let j = 0; j < 4; j++) {
+        let readFromIndex = (Math.random() * randomText.length); 
+        returnText += randomText.charAt(readFromIndex);
+    }
+    console.log(returnText);
+    document.getElementById("openText").innerHTML = "[" + returnText.substring(0,4) + "]";
+    returnText = 0;
+}
+*/
